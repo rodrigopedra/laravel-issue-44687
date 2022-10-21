c79b78e15fa3a91959d2885fb1e98938c77bdec9
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2022-10-21 19:58:27 -0300

    issue code

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/', function() {
+    request()->merge(['a' => 'c']);
+
+    return request()->all();
+});
+
