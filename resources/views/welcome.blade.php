86be9a17c54ea33427728b95990e9c110053d43f
Irakli Tchitadze <irakli.tchitadze@gmail.com>
2022-10-24 11:20:52 -0400

    Reproduced the request()->merge() issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
 </head>
 <body>
 <h2>Plain</h2>
-
 <form action="/" method="POST">
     @csrf
     <input type="text" name="a" value="b">
@@ -18,8 +17,14 @@
 <hr>
 
 <h2>With validation</h2>
-
 <form action="/validate" method="POST">
+    @csrf
+    <input type="text" name="a" value="b">
+    <button type="submit">send</button>
+</form>
+
+<h2>With Controller</h2>
+<form action="/with-controller" method="POST">
     @csrf
     <input type="text" name="a" value="b">
     <button type="submit">send</button>
